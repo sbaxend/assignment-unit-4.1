1d74e3f3593580850144620dfd8dd3dea560d2a1
Seth Baxendell <Sethbaxendell2013@hotmail.com>
2023-01-30 09:54:05 -0500

    got the adding function working

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,16 +15,16 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  return console.log('Hello Seth');
 }
 // Remember to call the function to test
-
-
+helloName()
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers( firstNumber, secondNumber ) {
   // return firstNumber + secondNumber;
+  console.log (firstNumber + secondNumber)
 }
-
+addNumbers(348,1200)
 
 // 4. Function to multiply three numbers & return the result
 function multiplyThree( ){
