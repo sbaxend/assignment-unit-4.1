afb9ec814603f8d38a0632acea2d0847693368f5
Seth Baxendell <Sethbaxendell2013@hotmail.com>
2023-01-31 09:01:53 -0500

    stretchgoal8

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -76,7 +76,7 @@ function getLast( array ) {
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
-let theArray = [12, 8, 75, 13]
+let theArray = [12, 8, 75, 13, -3, -12]
 function findValue( value, array ){
   let i = 0;
   while (i < array.length) {
@@ -88,15 +88,10 @@ function findValue( value, array ){
   return false;
 }
 
-// i couldnt figure it out doing any other loop
+// i couldnt figure it out doing any other type of loops
 
   console.log(findValue(75, theArray));
   console.log(findValue(24, theArray));
-
-    //if (value === 0) 
-    //return false;
-    //else (value )
-
 
 // ----------------------
 // Stretch Goals
@@ -104,21 +99,40 @@ function findValue( value, array ){
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  return letter === string[0]
 }
+let newWord = 'tomatos'
+console.log(isFirstLetter('x', newWord))
+console.log(isFirstLetter('t', newWord))
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
-  let sum = 0
+function sumAll(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
   // TODO: loop to add items
   return sum;
 }
+console.log(sumAll(theArray))
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
+function returnPositive( array ) {
+  let positiveArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 0) {
+      positiveArray.push(array[i])
+    }
+  } 
+    return positiveArray;
+    }
+  
+
+console.log(returnPositive(theArray))
 
 
 
